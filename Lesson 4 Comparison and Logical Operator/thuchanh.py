b558ffb38130c066561348bcde2ab321ab528e49
Đỗ Văn Dũng <143073328+dunnn25@users.noreply.github.com>
2025-08-26 21:15:11 +0700

    Thuc Hanh

---
+++ Lesson 4 Comparison and Logical Operator/thuchanh.py
@@ -0,0 +1,18 @@
+# # Comparison operators
+# print(20 == 20)
+print("Anh" == "em")
+
+# # not equal
+print(20 != 10)
+print("anh" != "em")
+
+# # >, >=, <, <=
+print(10 > 10)
+
+# logical operator
+num = 15
+print("Number is in the range [10, 20]: ", num >= 10 and num <= 20)
+print("Number is in the range [10, 20]: ", 10 <= num <= 20)
+bol = False
+print(not bol)
+print(not (6 > 5))
