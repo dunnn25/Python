77a6e16b9c65748c83d8136645f7130df4aa99f2
Đỗ Văn Dũng <143073328+dunnn25@users.noreply.github.com>
2025-08-26 21:10:53 +0700

    Thuc Hanh

---
+++ Lesson 2 Python Arithmetic Operator/thuchanh.py
@@ -0,0 +1,21 @@
+# Addition +\
+print(3 + 10)
+# Subtraction -
+print (10 - 3)
+# Multiplication *
+print (3*3)
+# Division /
+print (11/2)
+print (int(11/2))
+# Modulus %
+print (9 % 2)
+print (-10 % 3)
+# Exponentiation **
+print (3**3)
+# Floor division //       N = D * (N // D) + (N % D) (phép chia lấy phần nguyên)
+print (10 // 3)     # floor division: nhận được số nguyên lớn nhất nhỏ hơn hoặc bằng kết quả
+
+int_num = 10
+print(int_num)
+int_num += 5
+print(int_num )
