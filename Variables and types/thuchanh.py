9058bcf56c0b83c66b819dba023b4c0d9e513d9b
Đỗ Văn Dũng <143073328+dunnn25@users.noreply.github.com>
2025-08-26 21:00:10 +0700

    Thuchanh

---
+++ Variables and types/thuchanh.py
@@ -0,0 +1,25 @@
+# # Variables
+# # Number int, float, complex
+# # int
+# int_num = 10
+# print(int_num)
+# print(type(int_num))
+
+# # Float
+# float_num = 10.2
+# print(float_num)
+# print(type(float_num))
+
+# # Boolean - kieu ly luan True, Flase
+# print(6 == 8)
+# print(type(6 == 8))
+
+# # String - '', ""
+# my_str = "Hello"
+# print(my_str)
+# print(type(my_str))
+
+# my_str = """Hellloo
+# tada"""
+# print(my_str)
+
