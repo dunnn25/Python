a6153931f4ecd70cdb9c40482b24eb65084965ff
Đỗ Văn Dũng <143073328+dunnn25@users.noreply.github.com>
2025-09-05 23:52:26 +0700

    Thuchanh

---
+++ Lesson 9 If-else/Thuchanh1.py
@@ -0,0 +1,24 @@
+# else
+num = int(input("Enter a integer number:"))
+if num % 2 == 0:
+    print(f"So nhập vào {num} là số chẵn")
+else:
+    print(f"So nhập vào {num} là số lẻ")
+
+
+# Elif
+score = float(input("Nhap diem mon hoa cua ban: "))
+if score > 8. :
+    print("Đạt loại giỏi")
+elif score > 6. :
+    print("Đạt loại khá")
+else:
+    print("Đạt loại yếu")
+
+
+# Trả về true/ false
+x = 0
+if x:
+    print("X không phải là 0")
+else:
+    print("X là 0")
