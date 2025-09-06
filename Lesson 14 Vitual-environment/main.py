cfc9a4ed7f5071f6dd474ed0e51987fdf232cce0
Đỗ Văn Dũng <143073328+dunnn25@users.noreply.github.com>
2025-09-06 23:55:34 +0700

    Add files via upload

---
+++ Lesson 14 Vitual-environment/main.py
@@ -0,0 +1,13 @@
+"# tạo môi trường ảo"
+## miniconda, vitualenv, pyenv, venv
+"# ----------------------------------------------------------------- #"
+"# Cách tạo môi trường ảo bằng venv:"
+# python -m venv (Tên môi trường ảo muốn cài đặt) VD: "python -m venv myenv"
+# .\myenv\Scripts\activate (Cách active môi trường ảo bằng venv)
+# deactivate (khôi phục cài đặt)
+"# ----------------------------------------------------------------- #"
+'# Cách tạo môi trường ảo với conda'
+# Bước 1: vào conda promt 
+# Bước 2: Gõ lệnh "conda create --name (tên môi trường ảo) python=(Bản python muốn cài đặt cho môi trường ảo mới)"
+# Bước 3: Gõ lệnh "conda activate (tên môi trường ảo đã cài đặt)"
+# Môi trường ảo mặc định là "Base"
