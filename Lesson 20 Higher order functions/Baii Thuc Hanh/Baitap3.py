e4fa0ef29cfcc7c4217ad87f658f5f9ca76210d8
Đỗ Văn Dũng <143073328+dunnn25@users.noreply.github.com>
2025-09-12 15:16:34 +0700

    Add files via upload

---
+++ Lesson 20 Higher order functions/Baii Thuc Hanh/Baitap3.py
@@ -0,0 +1,9 @@
+people = [
+    {"name": "Alice", "age": 30},
+    {"name": "Bob", "age": 25},
+    {"name": "Charlie", "age": 35}
+]
+sort = sorted(people, key = lambda person:person['age'])
+print(sort)
+
+    
