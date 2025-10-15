fbda3cf5e33df1982b73de958eaddaa8ec8b9dac
Đỗ Văn Dũng <143073328+dunnn25@users.noreply.github.com>
2025-10-15 23:51:03 +0700

    Add files via upload

---
+++ Lesson 29 Tip and trick/Pickle in python/run.py
@@ -0,0 +1,20 @@
+import pickle
+
+dict = {
+    "name": "Dung",
+    "age": 21,
+    "Job": "AI Enginer"
+}
+# save python object to .pickle file
+# with open("save.pickel", "wb") as stream:
+    # pickle.dump(dict, stream)
+
+# load content of .pickle file   
+# with open("save.pickel","rb") as stream:
+#     my_dict = pickle.load(stream)
+# print(my_dict)
+# print(type(my_dict))
+ 
+ 
+# pricle_ob = pickle.dumps(dict)
+# new_dict = pickle.loads(pricle_ob)
